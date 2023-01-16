373f7c85f3f27d0a68bf4ec28a53fa0592c25c29
Leon Menzies <leon.menzies@hotmail.com>
2023-01-16 20:17:51 +1300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Leon Menzies on 16/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
